4bb8349d4920be3db66f67c1bdfc0f21ba32e579
marius-tverijonas <marius@veleza.com>
2018-12-15 14:39:56 +0200

    Add NodeJs Expess as backend

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../frontend/build')));
+
+// Answer API requests.
+app.get('/api', function (req, res) {
+    res.set('Content-Type', 'application/json');
+    res.send('{"message":"Hello from the custom server!"}');
+});
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+});
+
+app.listen(PORT, function () {
+    console.error(`Listening on port ${PORT}`);
+});
+
